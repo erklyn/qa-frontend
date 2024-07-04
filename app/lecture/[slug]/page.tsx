353a39bde44850981aa8388a5da792c20aad3c69
Utku Enes GURSEL <uenesgursel@gmail.com>
2024-07-04 21:52:22 +0300

    lecture page done?

---
+++ app/lecture/[slug]/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+
+export type Question = {
+    name: string;
+    question: string;
+};
+
+async function getLectureQuestions(slug: string): Promise<Question[]> {
+    const url = `http://localhost:3000/${slug}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json: Question[] = await response.json();
+        return json;
+    } catch (error) {
+        return [];
+    }
+}
+
+export default async function Page({
+    params: { slug },
+}: {
+    params: { slug: string };
+}) {
+    const [name, setName] = useState("");
+    const [question, setQuestion] = useState("");
+    const questions = await getLectureQuestions(slug);
+    return (
+        <>
+            <div className="width-full grid grid-cols-2 gap-3 p-3">
+                <div>
+                    {!!questions.length &&
+                        questions.map((question, id) => (
+                            <div
+                                key={id}
+                                className="rounded-md shadow-xl bg-slate-400 text-black p-2  m-2 gap-2"
+                            >
+                                <h3 className="pl-2 text-xl">{question.question}</h3>
+                                <h3 className="pl-2">{question.name}</h3>
+                            </div>
+                        ))}
+                </div>
+                <div>
+                    <div className="flex flex-col gap-3">
+                        <input
+                            className="text-center rounded-lg shadow-xl text-xl  w-full h-12  border-2 border-white"
+                            placeholder="Adiniz"
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value)
+                            }}
+                        />
+                        <textarea
+                            className="rounded-lg shadow-xl text-xl  w-full h-24  border-2 border-white"
+                            placeholder="Sorunuz"
+                            value={question}
+                            onChange={(e) => {
+                                setQuestion(e.target.value)
+                            }}
+                        />
+                        <button className="border-2 border-blue-300 bg-emerald-200 hover:border-dotted rounded-lg h-12 mx-12 hover:border-green-400" onClick={() => { }}>Sorunu gonder yayin sonunda cevaplamaya calisayim</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
